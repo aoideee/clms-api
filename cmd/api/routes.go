738f01505f219be300d53c39cfb825c49b6795bf
Tysha Daniels <2023158020@ub.edu.bz>
2026-03-22 08:15:46 -0600

    feat(api): implement application routing and healthcheck endpoint
    Set up a new HTTP servemux and updated the server to use  as its handler. Included a new  endpoint to easily verify that the Community Library Management System (CLMS) API is up and responding to requests.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,3 +1,20 @@
 // Filenaeme: routes.go
 
 package main
+
+import (
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	// Create a new servemux and register the handler functions for the different routes
+	mux := http.NewServeMux()
+
+	// Register the handler function for the "/v1/healthcheck" endpoint
+	mux.HandleFunc("/v1/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("The Community Library Management System (CLMS) is up and running!"))
+	})
+
+	return mux
+
+}
