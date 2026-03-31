b7ccdd26ee3ab459396ac0c6050296ced33a0bff
Tysha Daniels <2023158020@ub.edu.bz>
2026-03-30 19:47:27 -0600

    feat: add authentication schema and user roles

--- migrations/000001_create_clms_schema.down.sql
+++ migrations/000001_create_clms_schema.down.sql
@@ -3,7 +3,7 @@ DROP INDEX IF EXISTS idx_reservation_book_status;
 DROP INDEX IF EXISTS idx_fine_member_paid;
 DROP INDEX IF EXISTS idx_loans_member_return;
 DROP INDEX IF EXISTS idx_copy_barcode;
-DROP INDEX IF EXISTS idx_members_email;
+DROP INDEX IF EXISTS idx_users_email;
 DROP INDEX IF EXISTS idx_books_isbn;
 
 -- Drop tables in reverse order of creation
@@ -13,8 +13,11 @@ DROP TABLE IF EXISTS Reservation;
 DROP TABLE IF EXISTS Copy;
 DROP TABLE IF EXISTS BookGenre;
 DROP TABLE IF EXISTS BookAuthor;
+DROP TABLE IF EXISTS Staff;
 DROP TABLE IF EXISTS Member;
 DROP TABLE IF EXISTS Branch;
 DROP TABLE IF EXISTS Books;
 DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS Author;
+DROP TABLE IF EXISTS Tokens;
+DROP TABLE IF EXISTS Users;
